0f1157d6b643e1176ea6e8f18c8c2ab417bf2989
rahul rangarajan <137828612+rrahul002@users.noreply.github.com>
2024-08-02 13:10:43 -0400

    Update LinkedLists.py (has_loop())
    added has_loop function

--- LinkedLists.py
+++ LinkedLists.py
@@ -129,3 +129,13 @@ class LinkedList:
             slow = slow.next
             fast = fast.next.next
         return slow
+
+    def has_loop(self):
+        slow = self.head
+        fast = self.head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
